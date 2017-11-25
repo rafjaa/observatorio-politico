653c63101f0cd275ec5f40744830d09d6a687a55
Rafael Alencar <rafjaa@gmail.com>
2017-11-25 13:17:49 -0200

    Coletor de notícias

--- src/crawler/coleta_noticias.py
+++ src/crawler/coleta_noticias.py
@@ -1,7 +1,24 @@
+import random
+
 from bs4 import BeautifulSoup
 import requests
 
 from settings import *
 
 
+def parser_g1(soup):
+    return soup
 
+
+def obtem_noticia(url, parser):
+    user_agent = random.choice(USER_AGENTS)
+    html = requests.get(url, headers={'User-Agent': user_agent}).text
+    return parser(BeautifulSoup(html, 'html.parser'))
+
+
+
+if __name__ == '__main__':
+    print(obtem_noticia(
+        'https://g1.globo.com/pr/parana/noticia/publicitario-diz-que-fez-repasses-de-propina-para-ex-presidente-da-petrobras-a-pedido-da-odebrecht.ghtml',
+        parser_g1
+        ))
