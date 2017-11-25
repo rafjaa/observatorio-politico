a5160e60393c77233534b698a478effe9a4480aa
Rafael Alencar <rafjaa@gmail.com>
2017-11-25 13:12:24 -0200

    Settings modularizados

--- src/crawler/coleta_links.py
+++ src/crawler/coleta_links.py
@@ -8,34 +8,7 @@ import random
 from bs4 import BeautifulSoup
 import requests
 
-
-PASTA_LINKS = './links/'
-
-# Agentes de usuário, via http://useragentstring.com/pages/useragentstring.php
-USER_AGENTS = [
-    # Chrome
-    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36',
-
-    # FireFox
-    'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
-    'Mozilla/5.0 (Android; U; Android; pl; rv:1.9.2.8) Gecko/20100202 Firefox/3.5.8',
-    'Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0',
-
-    # IE / Edge
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
-
-    # Safari
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A',
-
-    # Opera
-    'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
-
-    # RockMelt
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) RockMelt/0.9.58.494 Chrome/11.0.696.71 Safari/534.24'
-]
+from settings import *
 
 
 def parser_g1(html):
---
+++ src/crawler/coleta_noticias.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+from settings import *
+
+
+
